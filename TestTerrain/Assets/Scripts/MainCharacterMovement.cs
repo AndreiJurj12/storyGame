91843a2ab74308a17b0e4b214975171dfb10d4c1
jurjstyle <jurj.andrei12@gmail.com>
2020-01-13 01:01:44 +0200

    Add Test Project

---
+++ TestTerrain/Assets/Scripts/MainCharacterMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float movementSpeed = 10f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var camera = Camera.main;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 camForward = Vector3.Scale(camera.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 camRight = Vector3.Scale(camera.transform.right, new Vector3(1, 0, 1)).normalized;
+        m_Movement = vertical * camForward + horizontal * camRight;
+        m_Movement.Normalize();
+
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+
+
+        //Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        Vector3 desiredMoveDirection = vertical * camForward + horizontal * camRight;
+        m_Rotation = Quaternion.LookRotation(desiredMoveDirection);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude * movementSpeed);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
---
+++ TestTerrain/Assets/Scripts/WayPointPatrol.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class WayPointPatrol : MonoBehaviour
+{
+    Animator m_Animator;
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+
+    bool isWalking = true;
+    int m_CurrentWaypointIndex = 0;
+
+    private void scriptGenerateWayPoints()
+    {
+        int numberWaypoints = Random.Range(2, 4);
+        waypoints = new Transform[numberWaypoints];
+
+        int y = 15; //standard
+
+        GameObject beginObject = new GameObject();
+        beginObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        waypoints[0] = beginObject.transform;
+
+        for (int i = 1; i < numberWaypoints; i += 1)
+        {
+            while (true)
+            {
+                int x = Random.Range(5, 100);
+                int z = Random.Range(400, 500);
+
+                GameObject emptyObject = new GameObject();
+                emptyObject.transform.position = new Vector3(x, y, z);
+
+                NavMeshPath path = new NavMeshPath();
+                navMeshAgent.CalculatePath(emptyObject.transform.position, path);
+
+                if (path.status == NavMeshPathStatus.PathComplete)
+                {
+                    waypoints[i] = emptyObject.transform;
+                    break;
+                }
+                else
+                {
+                    Destroy(emptyObject);
+                }
+
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        if (waypoints.Length == 0)
+        {
+            this.scriptGenerateWayPoints();
+        }
+
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWalking)
+        {
+            if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+            {
+                StartCoroutine(WaitIdling());
+            }
+        }
+    }
+
+    IEnumerator WaitIdling()
+    {
+        isWalking = false;
+        m_Animator.SetBool("IsWalking", isWalking);
+        navMeshAgent.isStopped = true;
+
+        yield return new WaitForSeconds(2);
+
+        m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+        Debug.Log("Index: " + m_CurrentWaypointIndex);
+
+        navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        navMeshAgent.isStopped = false;
+        isWalking = true;
+        m_Animator.SetBool("IsWalking", isWalking);
+    }
+}
